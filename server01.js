1877d5aa026df67153c320d34e20078dcd23fc35
charlenezell <charlenezell@gmail.com>
2017-08-28 23:45:20 +0800

    bk

--- server01.js
+++ server01.js
@@ -7,79 +7,13 @@ let createDBObject = require("./db.js");
 var router = jsonServer.router(createDBObject());
 var typeis = require('type-is');
 
-// server.use('/qq', proxy({
-//     target: 'http://qq.100bt.com',
-//     changeOrigin: true,
-//     pathRewrite: {
-//         '^/qq': ''
-//     },
-//     onProxyRes: function (proxyRes, req, res) {
-//         if (typeis(proxyRes, ['application/json'])) {
-//             hpj(res, proxyRes.headers['content-encoding'], (body) => {
-//                 if (body) {
-//                     body.aaa = "zellinject"
-//                 }
-//                 return body;
-//             })
-//         } else if (typeis(proxyRes, ['html'])) {
-//             console.log("html")
-//         } else if (typeis(proxyRes, ['application/javascript'])) {
-//             hpjp(req,res, proxyRes.headers['content-encoding'], (body) => {
-//                 if (body) {
-//                     body.aaa = "zellinject"
-//                 }
-//                 return body;
-//             })
-//         }
-//     }
-// }));
-
-server.use('/www', proxy({
-    target: 'http://www.100bt.com',
+server.use('/hs', proxy({
+    target: 'http://www.hushuoapp.com',
     changeOrigin: true,
     pathRewrite: {
-        '^/www': ''
-    }
-}));
-
-server.use('/my', proxy({
-    target: 'http://my.100bt.com',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/my': ''
-    }
-}));
-
-// server.use('/service', proxy({
-//     target: 'http://service.100bt.com',
-//     changeOrigin: true,
-//     pathRewrite: {
-//         '^/service': ''
-//     }
-// }));
-server.use('/service', proxy({
-    target: 'http://service.100bt.com',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/service': ''
+        '^/hs': ''
     },
     onProxyRes: function (proxyRes, req, res) {
-        res.writeHead = function(){
-            if( proxyRes.headers && proxyRes.headers[ 'content-length' ] ){
-                res.setHeader(
-                    'content-length',
-                    parseInt( proxyRes.headers[ 'content-length' ], 10 ) + scriptElm.length
-                );
-            }
-
-            // This disables chunked encoding
-            res.setHeader( 'transfer-encoding', '' );
-
-            // Disable cache for all http as well
-            res.setHeader( 'cache-control', 'no-cache' );
-
-            _writeHead.apply( this, arguments );
-        };
         if (typeis(proxyRes, ['application/json'])) {
             hpj(res, proxyRes.headers['content-encoding'], (body) => {
                 if (body) {
@@ -90,17 +24,83 @@ server.use('/service', proxy({
         } else if (typeis(proxyRes, ['html'])) {
             console.log("html")
         } else if (typeis(proxyRes, ['application/javascript'])) {
-
             hpjp(req,res, proxyRes.headers['content-encoding'], (body) => {
                 if (body) {
                     body.aaa = "zellinject"
                 }
                 return body;
-            });
-
+            })
         }
     }
 }));
+
+// server.use('/www', proxy({
+//     target: 'http://www.100bt.com',
+//     changeOrigin: true,
+//     pathRewrite: {
+//         '^/www': ''
+//     }
+// }));
+
+// server.use('/my', proxy({
+//     target: 'http://my.100bt.com',
+//     changeOrigin: true,
+//     pathRewrite: {
+//         '^/my': ''
+//     }
+// }));
+
+// server.use('/service', proxy({
+//     target: 'http://service.100bt.com',
+//     changeOrigin: true,
+//     pathRewrite: {
+//         '^/service': ''
+//     }
+// }));
+// server.use('/service', proxy({
+//     target: 'http://service.100bt.com',
+//     changeOrigin: true,
+//     pathRewrite: {
+//         '^/service': ''
+//     },
+//     onProxyRes: function (proxyRes, req, res) {
+//         res.writeHead = function(){
+//             if( proxyRes.headers && proxyRes.headers[ 'content-length' ] ){
+//                 res.setHeader(
+//                     'content-length',
+//                     parseInt( proxyRes.headers[ 'content-length' ], 10 ) + scriptElm.length
+//                 );
+//             }
+
+//             // This disables chunked encoding
+//             res.setHeader( 'transfer-encoding', '' );
+
+//             // Disable cache for all http as well
+//             res.setHeader( 'cache-control', 'no-cache' );
+
+//             _writeHead.apply( this, arguments );
+//         };
+//         if (typeis(proxyRes, ['application/json'])) {
+//             hpj(res, proxyRes.headers['content-encoding'], (body) => {
+//                 if (body) {
+//                     body.aaa = "zellinject"
+//                 }
+//                 return body;
+//             })
+//         } else if (typeis(proxyRes, ['html'])) {
+//             console.log("html")
+//         } else if (typeis(proxyRes, ['application/javascript'])) {
+
+//             hpjp(req,res, proxyRes.headers['content-encoding'], (body) => {
+//                 if (body) {
+//                     body.aaa = "zellinject"
+//                 }
+//                 return body;
+//             });
+
+//         }
+//     }
+// }));
 
 server.use(router);
 
